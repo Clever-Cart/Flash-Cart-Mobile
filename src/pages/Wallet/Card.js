be0076248025ca910651c99227cd97d9ec4e6ad9
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2021-03-22 17:04:58 -0300

    Add remove card

--- src/pages/Wallet/Card.js
+++ src/pages/Wallet/Card.js
@@ -1,12 +1,24 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import {
+  View,
+  Text,
+  Alert,
+  StyleSheet,
+  TouchableOpacity
+} from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
+
+import * as firebase from 'firebase'
+import 'firebase/firestore'
 
 const styles = StyleSheet.create({
   card: {
     padding: 16,
     borderRadius: 8,
     backgroundColor: '#F97D7D',
-    justifyContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
     marginBottom: 16
   },
   name: {
@@ -17,12 +29,54 @@ const styles = StyleSheet.create({
   }
 })
 
-const Card = ({ card }) => (
-  <View style={styles.card}>
-    <Text style={styles.name}>Cartão de Crédito</Text>
-    <Text style={styles.name}>{card.name}</Text>
-    <Text style={styles.number}>{card.number}</Text>
-  </View>
-)
+const Card = ({ card }) => {
+
+  const handleDelete = async() => {
+    const db = firebase.firestore()
+    const user = firebase.auth().currentUser
+
+    try {
+      await db.collection('Users')
+        .doc(user.uid)
+        .collection('Cards')
+        .doc(card.number)
+        .delete()
+    } catch (e) {
+      console.error(e)
+    } 
+  }
+
+  const onDelete = () => {
+    Alert.alert(
+      'Você tem certeza?',
+      'Isso irá apagar permanentemente este meio de pagamento da sua carteira Flash Cart.',
+      [{
+        text: 'Confirmar',
+        onPress: handleDelete,
+        style: 'destructive'
+      }, {
+        text: 'Cancelar',
+        style: 'cancel'
+      }]
+    )
+  }
+
+  return (
+    <View style={styles.card}>
+      <View>
+        <Text style={styles.name}>Cartão de Crédito</Text>
+        <Text style={styles.name}>{card.name}</Text>
+        <Text style={styles.number}>{card.number}</Text>
+      </View>
+      <TouchableOpacity onPress={onDelete}>
+        <MaterialIcons
+          name='delete-outline'
+          size={36}
+          color='white'
+        />
+      </TouchableOpacity>
+    </View>
+  )
+}
 
 export default Card
